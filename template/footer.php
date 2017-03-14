d3126e46b8d6034bf4250ec32dff4f1d9e635eaa
5ganes <kh6ganesh@gmail.com>
2017-03-14 19:41:15 +0545

    hide sidebar on page load

--- template/footer.php
+++ template/footer.php
@@ -6,6 +6,11 @@
 			<script src="assets/js/util.js"></script>
 			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
 			<script src="assets/js/main.js"></script>
+			<script>
+				$(document).ready(function(){
+				  	$("#sidebar").addClass("inactive");
+				});
+			</script>
 
 	</body>
 </html>
